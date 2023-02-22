933ae0fdc68dc1a2fbac6e1ae5f6226f58084bee
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2023-02-22 10:08:06 +0900

    add mozc-ut / fcitx-mozc-ut / emacs-mozc-ut

---
+++ x11-packages/mozc-ut/fcitx5-mozc-ut.subpackage.sh
@@ -0,0 +1,9 @@
+TERMUX_SUBPKG_INCLUDE="
+lib/fcitx5
+share/metainfo
+share/icons
+share/fcitx5
+"
+
+TERMUX_SUBPKG_DEPENDS="fcitx5, gtk2, qt5-qtbase"
+TERMUX_SUBPKG_DESCRIPTION="Mozc input method for fcitx"
